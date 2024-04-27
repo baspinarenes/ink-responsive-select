3c01238a0dee6b228fd5de746b589aba144528f8
Enes Başpınar <enes.baspinar@trendyol.com>
2024-04-27 22:42:07 +0300

    Do somethings

--- TODO.md
+++ TODO.md
@@ -1,3 +1,5 @@
 # TODO:
 
 - Make it a simple stand-alone CLI tool. Perform the operation given after selection.
+- Add log workspace update for project not found.
+- Change way of getting projects list.
--- source/components/instructions.tsx
+++ source/components/instructions.tsx
@@ -4,7 +4,7 @@ import {Box, Text} from 'ink';
 export const Instructions: React.FC = () => {
 	return (
 		<Box marginBottom={1}>
-			<Text italic color="gray">
+			<Text italic dimColor>
 				Press {'<Space>'} to select, {'<Enter>'} to submit, {'<a>'} to
 				select/deselect all, {'<q>'} to exit.
 			</Text>
